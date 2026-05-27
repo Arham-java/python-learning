e457fc4bbc94f1f4f9005bed9c4cbb12329a260e
Arham_Jain <helloarham596@gmail.com>
2026-05-27 16:06:54 +0530

    password and id secured

--- Day_47(amazon_price_tracker)/price.py
+++ Day_47(amazon_price_tracker)/price.py
@@ -2,6 +2,11 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
 url = "https://appbrewery.github.io/instant_pot/"
 response = requests.get(url)
 
@@ -13,15 +18,25 @@ title = soup.find(id="productTitle").get_text().strip()
 
 price_as_floating=float(element)
 
-
+my_mail = os.getenv("MY_EMAIL")
+password = os.getenv("PASSWORD")
+SMTP_ADDRESS = os.getenv("SMTP_ADDRESS")
 
 
 if(price_as_floating<=100.00):
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+    message = f"""Subject: Price Drop Alert!
+    The product:
+    {title}
+    is now available for ${price_as_floating}
+    Buy here:
+    {url}
+    """
+
+    with smtplib.SMTP(SMTP_ADDRESS) as connection:
         connection.starttls()
         connection.login(user=my_mail,password=password)
         connection.sendmail(
             from_addr=my_mail,
-            to_addrs="helloarham596@gmail.com",
-            msg=f"subject:price drop!! \n\nThe product {title} is now {price_as_floating} link: {url}".encode("utf-8")
+            to_addrs="cse.23bcse65@silicon.ac.in",
+            msg=message.encode("utf-8")
         )
